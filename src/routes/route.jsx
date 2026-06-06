786235d512e97ab67686fe9e7bcddb122e334165
Ji Jetu <jijetu2@gmail.com>
2026-06-06 09:48:19 +0600

    updated

--- src/routes/route.jsx
+++ src/routes/route.jsx
@@ -1,15 +1,13 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import Mainlayout from "../layout/Mainlayout";
 import Home from "../pages/Home";
 import { TermsPage } from "../pages/TermsPage";
 import { PrivacyPage } from "../pages/PrivacyPage";
-import ErrorPage from "../pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Mainlayout />,
-    errorElement: <ErrorPage />,
     children: [
       {
         index: true,
@@ -25,10 +23,38 @@ const router = createBrowserRouter([
       },
       {
         path: "*",
-        element: <ErrorPage />,
+        element: <Navigate to="/" replace />,
       },
     ],
   },
 ]);
 
 export default router;
+
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <Mainlayout />,
+//     errorElement: <ErrorPage />,
+//     children: [
+//       {
+//         index: true,
+//         element: <Home />,
+//       },
+//       {
+//         path: "terms",
+//         element: <TermsPage />,
+//       },
+//       {
+//         path: "privacy",
+//         element: <PrivacyPage />,
+//       },
+//       {
+//         path: "*",
+//         element: <ErrorPage />,
+//       },
+//     ],
+//   },
+// ]);
+
+// export default router;
